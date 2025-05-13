706966c4852ee661504741a9751c06a3d25d4820
JuanCE7 <juancastiestrella@gmail.com>
2025-05-13 11:47:56 -0500

    feat home page with tailwind and prime ng

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    if (element) {
+      element.classList.toggle('my-app-dark');
+    }
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonModule, FloatLabelModule, InputTextModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
 }
