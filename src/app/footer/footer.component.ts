a98a00ecb4235eee1724665a6020c0dc8bf3d322
JuanCE7 <juancastiestrella@gmail.com>
2025-05-13 11:54:21 -0500

    update order

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  template: `<footer>
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-center items-center">
+        <p class="text-sm">
+          &copy; 2025 DevRex CRUD. Todos los derechos reservados.
+        </p>
+      </div>
+    </div>
+  </footer>`,
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
